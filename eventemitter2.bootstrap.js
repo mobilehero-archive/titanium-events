4abd13f307735e4b1d3f9914ff4b8fa8aedf1af0
Brenton House <brenton.house@gmail.com>
2019-10-08 21:25:11 -0700

    fixed bootstrap file

--- eventemitter2.bootstrap.js
+++ eventemitter2.bootstrap.js
@@ -1,6 +1,6 @@
 // code for SDK < 8.1.0.GA
-require('/ti.internal/extensions/binding')('events', '/node_modules/@titanium/events');
-require('/ti.internal/extensions/binding')('eventemitter2', '/node_modules/@titanium/events');
+// require('/ti.internal/extensions/binding')('events', '/node_modules/@titanium/events');
+// require('/ti.internal/extensions/binding')('eventemitter2', '/node_modules/@titanium/events');
 
 // Code for SDK >= 8.1.0.GA
 global.binding.register('events', require('@titanium/events'));
